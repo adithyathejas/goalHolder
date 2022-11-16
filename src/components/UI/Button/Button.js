81c20aaf73e6bc06dc27c64d2b03b24121fc6737
adithyathejas <adithyathejus00@gmail.com>
2022-11-16 13:25:12 +0530

    goal holder created

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Button.css';
+
+const Button = props => {
+  console.log(props.className)
+  return (
+    <button type={props.type} className={`button ${props.className}`} onClick={props.onClick} >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
